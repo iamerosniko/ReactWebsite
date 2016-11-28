243ac8e504266c16eb1ecc8d501b7b8f92b4d774
Eros Niko Alvarez <iamerosniko@yahoo.com>
2016-11-28 10:46:48 -0500

    fix

--- App.jsx
+++ App.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
+import {Projects} from './components/Projects.jsx'
+
 var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class App extends React.Component {
-
-   render() {
-      return (
-         <div>
-            <ReactCSSTransitionGroup 
-                transitionName = "example"
-                transitionAppear = {true} 
-                transitionAppearTimeout = {1000}
-                transitionEnter = {false} 
-                transitionLeave = {false}
-                >
-					
-               <h1>My Element...</h1>
-            </ReactCSSTransitionGroup>
-         </div>      
-      );
-   }
+    render() {
+        return (
+            <div>
+                <Projects />
+            </div>    
+        );
+    }
 }
 
 export default App;
---
+++ components/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Projects extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3> Project Lists </h3>
+            </div>    
+        );
+    }
+}
+
+export default Projects;
